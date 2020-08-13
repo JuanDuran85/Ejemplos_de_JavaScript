f23ea9e840da8cf60014a1fd0d7fa1189556d488
JuanDuran85 <duran85juan@gmail.com>
2020-08-13 11:45:01 -0400

    trabajando con modulos en es6

---
+++ modulos_1/modulo_base.js
@@ -0,0 +1,7 @@
+// con esto se importa todo el archivo completo con las variables y rutinas que tengan indicado el export 
+import * as externo from './modulo_dos.mjs';
+
+//esta es otra forma de importar variables una por una especificando desde el modulo cual queremos importar
+//import obj2, {obj1, arr1} from './modulo_dos.mjs'
+
+console.log(externo);
---
+++ modulos_1/modulo_dos.mjs
@@ -0,0 +1,15 @@
+export const obj1 = {
+    nombre: "Taty",
+    clase: "Perro",
+    raza: "Pastor aleman"
+};
+
+export const arr1 = [43,6,"2",true,[4,5],{nombre: "Kittie", edad: 11}];
+
+let obj2 = [
+    {pais: "Chile", visitado: true},
+    {pais: "Venezuela", visitado: true},
+    {pais: "Argentina", visitado: false}
+]
+
+export default obj2;
