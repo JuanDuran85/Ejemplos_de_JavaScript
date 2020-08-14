08269c9ee6dc31a682abea8d87c6db167eb8b972
JuanDuran85 <duran85juan@gmail.com>
2020-08-14 12:11:50 -0400

    trabajando con string

---
+++ string/script.js
@@ -0,0 +1,24 @@
+// valores primitivos, a estos no se le asignan propiedades
+
+//primitiva
+let nombre = "Juan";
+
+//objeto
+let name = new String("Juan"); //esto retorna un objeto
+console.log(typeof name);
+
+//-----------------------------------------------------------------------------
+
+//escaping
+console.log("mensaje con escaping: \"JavaScript\" \\"); 
+console.log('mensaje con escaping: \'JavaScript\''); 
+console.log('mensaje con escaping: "JavaScript"');
+console.log('\n mensaje con escaping: "\\n"');
+console.log('\r\n mensaje con escaping: "\\r\\n"');
+console.log('\t mensaje con escaping: "\\t"');
+
+//---------------------------------------------------
+/* El método padStart() rellena la cadena actual con una cadena dada (repetida eventualmente) de modo que la cadena resultante alcance una longitud dada. El relleno es aplicado desde el inicio (izquierda) de la cadena actual. */
+
+
+
