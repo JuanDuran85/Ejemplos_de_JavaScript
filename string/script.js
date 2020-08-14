540318f1248799de692da37a3efaa47f0909deba
JuanDuran85 <duran85juan@gmail.com>
2020-08-14 12:53:21 -0400

    trabajando con string

--- string/script.js
+++ string/script.js
@@ -20,5 +20,12 @@ console.log('\t mensaje con escaping: "\\t"');
 //---------------------------------------------------
 /* El método padStart() rellena la cadena actual con una cadena dada (repetida eventualmente) de modo que la cadena resultante alcance una longitud dada. El relleno es aplicado desde el inicio (izquierda) de la cadena actual. */
 
+let mes = "3";
+console.log(mes.padStart(2,"0"));
+mes = "11";
+console.log(mes.padStart(2,"0"));
+let numero = "1";
+console.log(numero.padEnd(2,"0"));
 
+//------------------------------------------------------------------------
 
