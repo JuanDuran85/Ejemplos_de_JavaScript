0716d7d7e7ab47d6b394b6a5e77bc74f5f401b6b
JuanDuran85 <duran85juan@gmail.com>
2020-09-17 11:49:39 -0300

    clases, herencia, patron modulo, closures

---
+++ herencias/herencia9/assets/js/script.js
@@ -0,0 +1,82 @@
+let sugerencias = (() => {
+
+    let musica = document.querySelector('#musica');
+    let peliculas = document.querySelector('#peliculas');
+    let series = document.querySelector('#series');
+   
+    return {
+        mostrandoMusica: (videoMostrar)=>{
+            musica.insertAdjacentHTML('afterbegin', videoMostrar);
+        },
+        mostrandoPelicula: (videoMostrar)=>{
+            peliculas.insertAdjacentHTML('afterbegin', videoMostrar);
+        },
+        mostrandoSeries: (videoMostrar)=>{
+            series.insertAdjacentHTML('afterbegin', videoMostrar);
+        }
+    }
+
+})();
+
+let videoMusica = `<iframe width="560" height="315" src="https://www.youtube.com/embed/VDd_sdAMbtE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`; 
+ 
+let videoPelicula = `<iframe width="560" height="315" src="https://www.youtube.com/embed/5PSNL1qE6VY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    
+let videoSerie = `<iframe width="560" height="315" src="https://www.youtube.com/embed/rlR4PJn8b8I" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+class Video {
+    constructor(videoMusica, videoPelicula, videoSeries){
+        this.videoMusica = videoMusica;
+        this.videoPelicula = videoPelicula;
+        this.videoSeries = videoSeries;
+    }
+
+    mostrarMusica(){
+        sugerencias.mostrandoMusica(this.videoMusica);
+    }
+
+    mostrarPelicula(){
+        sugerencias.mostrandoPelicula(videoPelicula);
+    }
+
+    mostrarSeries(){
+        sugerencias.mostrandoSeries(videoSerie);
+    }
+}
+
+class Musica extends Video {
+    constructor(videoMusica){
+        super(videoMusica);
+    }
+
+    mostrarMusica(){
+        sugerencias.mostrandoMusica(this.videoMusica);
+    }
+}
+
+class Pelicula extends Video {
+    constructor(videoPelicula){
+        super(videoPelicula);
+    }
+
+    mostrarPelicula(){
+        sugerencias.mostrandoPelicula(videoPelicula);
+    }
+}
+
+class Serie extends Video {
+    constructor(videoSeries){
+        super(videoSeries);
+    }
+
+    mostrarSeries(){
+        sugerencias.mostrandoSeries(videoSerie);
+    }
+}
+
+let musica = new Musica(videoMusica);
+musica.mostrarMusica();
+let pelicula = new Pelicula(videoPelicula);
+pelicula.mostrarPelicula();
+let serie = new Serie(videoSerie);
+serie.mostrarSeries();
