5691605fb8cd32dec5dfd63f6af08f6def8dac0b
JuanDuran85 <duran85juan@gmail.com>
2020-09-27 19:08:12 -0300

    agregando ventana modal

--- ConexionAPIRickMorty/assets/js/detallesPersonajes.js
+++ ConexionAPIRickMorty/assets/js/detallesPersonajes.js
@@ -1,14 +1,18 @@
 import Personajes from './personajes.js';
 
 export default class DetallesPersonajes extends Personajes {
-    constructor(id,name, gender, species, status){
+    constructor(id,name, status, species, gender, created, origin, location, episode){
         super(id);
         let _name = name;
         this.getName = () => _name;
         this.setName = (nuevo_name) => _name = nuevo_name;
-        this._gender = gender;
-        this._species = species;
         this._status = status;
+        this._species = species;
+        this._gender = gender;
+        this._created = created;
+        this._origin = origin;
+        this._location = location;
+        this._episode = episode;
     }
 
     get name(){
@@ -17,5 +21,28 @@ export default class DetallesPersonajes extends Personajes {
 
     set name(nuevo_name){
         this.setName(nuevo_name);
+    }
+
+    infoModal(){
+        return `
+            <ul>
+                <li><span>ID: ${this.id}</span></li>
+                <li><span>Nombre: ${this.name}</span></li>
+                <li><span>Genero: ${this._gender}</span></li>
+                <li><span>Especie: ${this._species}</span></li>
+                <li><span>Status: ${this._status}</span></li>
+                <li><span>Creado: ${this._created}</span></li>
+                <li><span>Origen: ${this._origin.name}</span></li>
+                <li><span>Cantidad de Episodios: ${this._episode.length}</span></li>
+            </ul>
+        `
+    }
+    
+    infoGeneral(){
+        return `
+            <ul>
+                <li><span>${this.id}</span></li>
+            </ul>
+        `
     }
 }
--- ConexionAPIRickMorty/assets/js/main.js
+++ ConexionAPIRickMorty/assets/js/main.js
@@ -19,18 +19,33 @@ let llamadoPersonajes = (()=>{
         try {
             let respuesta = await fetch(urlAPI+idPersonaje);
             let data = await respuesta.json();
-            const {id,name, gender, species, status } = data;
-            const infoPersonaje = new DetallesPersonajes(id,name, gender, species, status);
+            const {id,name, status, species, gender, created, origin, location, episode } = data;
+            const infoPersonaje = new DetallesPersonajes(id,name, status, species, gender, created, origin, location, episode);
             let detallePersonaje = document.querySelector(`#person-${idPersonaje}`);
             let detalle = `
-                <ul>
-                    <li><span>${infoPersonaje.id}</span></li>
-                    <li><span>${infoPersonaje.name}</span></li>
-                    <li><span>${infoPersonaje._gender}</span></li>
-                    <li><span>${infoPersonaje._species}</span></li>
-                </ul>
+                ${infoPersonaje.infoGeneral()}
+            `;
+            let ventanaModal = document.querySelector(`#personModal-${idPersonaje}`);
+            let modalPeronsaje = `                            
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">${infoPersonaje.name}</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ${infoPersonaje.infoModal()}
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
             `;
             detallePersonaje.innerHTML = detalle;
+            ventanaModal.innerHTML = modalPeronsaje;
         } catch (error) {
             console.error(error);
         }
@@ -42,10 +57,12 @@ let llamadoPersonajes = (()=>{
             const respuestaPersonajes = await personajes.results;
             respuestaPersonajes.forEach(element => {
                 resultados.innerHTML += `
-                    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-                        <img src="${element.image}" alt="${element.id}" class="img-fluid">
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 ">
+                        <img src="${element.image}" data-toggle="modal" data-target="#personModal-${element.id}" alt="${element.id}" class="img-fluid">
                         <div class="d-inline-block detallePersona" id="person-${element.id}">
                             ${llamandoDetalle(element.id)}
+                        </div>
+                        <div class="modal fade" id="personModal-${element.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         </div>
                     </div>
                 `;
